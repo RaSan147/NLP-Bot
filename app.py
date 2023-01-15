5ac5b2eed9070d4a886965d6517655eb1513ad02
RaSan <34002411+RaSan147@users.noreply.github.com>
2023-01-15 19:18:06 +0600

    x

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+
+
+
+
+
+
 import numpy as np
 import pandas as pd
 import preprocessor as p
 from tensorflow.keras.models import load_model
 import joblib
 from pathlib import Path 
-from PIL import Image
-import streamlit as st
-import SessionState
 
 #paths
-img_path = Path.joinpath(Path.cwd(),'images')
 artifacts_path = Path.joinpath(Path.cwd(),'model_artifacts')
 datasets_path = Path.joinpath(Path.cwd(),'dataset')
-
-#load images 
-center = Image.open(Path.joinpath(img_path,'center.jpg'))
-federer_image = Image.open(Path.joinpath(img_path,'federer.jpg'))
-nadal =Image.open(Path.joinpath(img_path,'Nadal.jpg'))
 
 #load artifacts 
 model = load_model(Path.joinpath(artifacts_path,'model-v1.h5'))
@@ -24,8 +23,6 @@ tokenizer_t = joblib.load(Path.joinpath(artifacts_path,'tokenizer_t.pkl'))
 vocab = joblib.load(Path.joinpath(artifacts_path,'vocab.pkl'))
 
 df2 = pd.read_csv(Path.joinpath(datasets_path,'response.csv'))
-
-ss = SessionState.get(is_startup=True) 
 
 def get_pred(model,encoded_input):
     pred = np.argmax(model.predict(encoded_input))
@@ -59,31 +56,14 @@ def botResponse(user_input):
     response = get_response(df2,pred)
     response = bot_response(response)
     
-    if ss.is_startup:
-        response = "Hi, I'm happy to have you here \nI have a lot to discuss about tennis"
-        ss.is_startup = False
-        return response
-
-    else:
-        return  response
+    print(response)
 
 def get_text():
-    input_text = st.text_input("You: ","type here")
+    input_text = input('Enter your question: ')
     df_input = pd.DataFrame([input_text],columns=['questions'])
     return df_input 
 
 
-
-st.sidebar.title("BoTennis")
-st.title("""
-BoTennis  
-BoTennis is a NLP bot trainned basic tennis corpus using  CNN achitecture
-""")
-
-st.image(center,width=700)
-st.sidebar.image(federer_image)
-st.sidebar.image(nadal,width=350)
-
-user_input = get_text()
-response = botResponse(user_input)
-st.text_area("Bot:", value=response, height=200, max_chars=None, key=None)
+while True:
+    df_input = get_text()
+    botResponse(df_input)
